14f32e19d2d223df4bd2d7d364e0f01b51e6882b
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-25 15:14:46 -0400

    add deckOfCards

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,7 +1,9 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
 import Card from './Card';
 import Button from './Button';
+import axios from 'axios';
+
+const DECK_CARDS_API_URL = "https://deckofcardsapi.com/api";
 
 /** DeckOfCards
  *
@@ -13,9 +15,72 @@ import Button from './Button';
  * Effects:
  *
  *
- * App --> DeckOfCards
+ * App --> DeckOfCards --> Card
+ * App --> DeckOfCards --> Button
  */
+
 function DeckOfCards() {
+  const initialDeck = {
+    isLoading: true,
+    data: null,
+    errors: null
+  };
+  const [deck, setDeck] = useState(initialDeck);
+  const [card, setCard] = useState(null);
+
+  console.debug("DeckOfCards ran, deck = ", deck);
+
+  useEffect(function fetchDeckWhenMounted() {
+    async function fetchDeck() {
+      try {
+        const response = await axios.get(`${DECK_CARDS_API_URL}/deck/new/`);
+        setDeck({
+          data: response.data,
+          isLoading: false,
+          errors: null,
+        });
+      } catch (err) {
+        setDeck({
+          data: null,
+          isLoading: false,
+          errors: err,
+        });
+      }
+    }
+    fetchDeck();
+  }, []);
+
+
+  async function drawCard() {
+    const response = await axios.get(
+      `${DECK_CARDS_API_URL}/deck/${deck.data.deck_id}/draw/?count=1`
+    );
+
+    const { image, code } = response.data.cards[0];
+    const remaining = response.data.remaining;
+    const card = { image, code, remaining };
+
+    setCard(card);
+  }
+
+  if (deck.isLoading) return <i>Loading...</i>;
+  else if (deck.errors) return <b>Oh no! {deck.errors}</b>;
+  return (
+    <div className="DeckOfCards">
+      {card !== null && card?.remaining > 0
+        ?
+        <div>
+          <Button drawCard={drawCard} />
+          <Card image={card.image} code={card.code} />
+        </div>
+        :
+        <Button drawCard={drawCard} />
+      }
+      {card?.remaining === 0 &&
+        <p style={{ color: "red" }}>Error: no cards remaining!</p>
+      }
+    </div>
+  );
 
 }
 
