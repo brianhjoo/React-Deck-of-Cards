467b4654ba60ed16cb4cb5a74b5893f3d2105b37
Brian Joo <brianhjoo@gmail.com>
2023-04-25 11:38:25 -0700

    saved file

---
+++ src/DeckOfCards.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Card from './Card';
+import Button from './Button';
+
+/** DeckOfCards
+ *
+ * Props: none
+ *
+ * State:
+ *  - deck: deck of cards from API - { isLoaded, data, error }
+ *
+ * Effects:
+ *
+ *
+ * App --> DeckOfCards
+ */
+function DeckOfCards() {
+
+}
+
+
+export default DeckOfCards;
