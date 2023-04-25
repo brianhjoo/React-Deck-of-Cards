ed27c1529bb5177bd4b39f80845fffaac79410cf
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-25 15:17:56 -0400

    add docstring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import DeckOfCards from './DeckOfCards';
 import './App.css';
 
+/** App
+ *
+ * Props none
+ * State none
+ *
+ * App --> DeckOfCards
+ */
+
 function App() {
   return (
     <div className="App">
